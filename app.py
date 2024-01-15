53023205e3100b7c18f7fbb9b0a90953f905bbd8
joshiw <155970886+joshiw@users.noreply.github.com>
2024-01-15 13:25:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from prettytable import PrettyTable
+import cowsay 
 app = Flask(__name__)
 bootstrap=Bootstrap(app)
 @app.route('/')
@@ -10,3 +11,11 @@ def index():
     return render_template('index.html')
 if __name__=='__main__':
     app.run(debug=True)
+my_apple =r''' 
+,--./,-.
+ / #      \
+|          |
+ \        /    hjw
+  `._,._,'
+'''
+cowsay.draw(my_apple)
