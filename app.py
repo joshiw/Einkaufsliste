8fd3dfdcc5033023cd1ea63cc79edfbc81c7551e
juliana2222 <155970469+juliana2222@users.noreply.github.com>
2024-01-15 12:23:29 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from prettytable import PrettyTable
+app = Flask(__name__)
+bootstrap=Bootstrap(app)
+@app.route('/')
+def index():
+    Einkaufsliste = PrettyTable(["Produkt","Menge","Gekauft"])
+    Einkaufsliste.add_row(["Äpfel","4"," "])
+    return render_template('index.html')
+if __name__=='__main__':
+    app.run(debug=True)
